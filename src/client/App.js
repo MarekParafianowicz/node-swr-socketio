a94b7ef3aab2cb01bd422034d71e5a2c81f45d98
marek parafianowicz <marek.parafianowicz@netguru.co>
2018-04-24 18:03:33 +0200

    Use styled components

--- src/client/App.js
+++ src/client/App.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+import Chat from './Chat';
 
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <p>Server Side Rendered</p>
-//       </div>
-//     );
-//   }
-// }
-const App = () => <div>Rendered on the server</div>;
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+  font-size: 40px;
+  background: linear-gradient(20deg, rgb(219, 112, 147), #daa357);
+`;
+// const AppContainer = styled.div`
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   position: fixed;
+//   width: 100%;
+//   height: 100%;
+//   font-size: 40px;
+//   background: linear-gradient(20deg, rgb(219, 112, 147), #daa357);
+// `;
+
+const App = () => {
+  return (
+    <AppContainer>
+      <h1> Chat test </h1>
+      <Chat/>
+    </AppContainer>
+  )
+}
 
 export default App;
--- src/server.js
+++ src/server.js
@@ -3,17 +3,21 @@ import React from 'react';
 import { renderToString } from 'react-dom/server';
 import App from './client/App';
 import Html from './client/Html';
+import { ServerStyleSheet } from 'styled-components';
 
 const port = 3000;
 const server = express();
 
 server.get('/', (req, res) => {
-  const body = renderToString(<App />);
+  const sheet = new ServerStyleSheet();
+  const body = renderToString(sheet.collectStyles(<App />));
+  const styles = sheet.getStyleTags();
   const title = 'Server side Rendering with Styled Components';
 
   res.send(
     Html({
       body,
+      styles,
       title
     })
   );
