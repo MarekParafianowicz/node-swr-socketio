df21b6c5712d714c0bc8fb3fbf5c4b51b62ca182
marek parafianowicz <marek.parafianowicz@netguru.co>
2018-04-24 14:21:26 +0200

    Initial commit

---
+++ src/client/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <p>Server Side Rendered</p>
+//       </div>
+//     );
+//   }
+// }
+const App = () => <div>Rendered on the server</div>;
+
+export default App;
